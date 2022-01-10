6995cde09b30b17304faf4e1e174d526ecb00866
Muhammad Usman <manijnb0333@gmail.com>
2022-01-10 16:29:16 +0500

    update

--- pages/contact.js
+++ pages/contact.js
@@ -40,7 +40,7 @@ function contact() {
           <h1 className={styles.headcont}>Have Any Query?</h1>
           <span className={styles.spen}>
             <p>
-              If you have any question or need help about<br></br> the service feel free to contact
+              If you have any questions, suggestions or just want to share some love,  <br></br> Need help about the service feel free to contact
               us We are excited to see you.
             </p>
           </span>
