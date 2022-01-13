0f439cada95daa31a4d619ee797742e2a9056d2b
Muhammad Usman <manijnb0333@gmail.com>
2022-01-13 10:01:33 +0500

    gtag update

--- public/gtag.js
+++ public/gtag.js
@@ -6,14 +6,14 @@ function Gtag() {
     return (
         <>
        
-<Script async src="https://www.googletagmanager.com/gtag/js?id=G-TKRL2H1FM8"></Script>
-<Script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-TKRL2H1FM8"></script>
+<script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments)}
   gtag('js', new Date());
 
   gtag('config', 'G-TKRL2H1FM8');
-</Script> 
+</script> 
         </>
     )
 }
