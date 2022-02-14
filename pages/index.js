a98ae02c1526f3793287bfc50935b30cf07c8545
Muhammad Usman <manijnb0333@gmail.com>
2022-02-14 12:31:41 +0500

    updates

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,6 @@ import Image from "next/image";
 import Head from "next/head";
 import Footer from "../Components/Footer";
 
-
 function index() {
   return (
     <>
@@ -13,7 +12,7 @@ function index() {
 
         <meta
           name="description"
-          content= "maniwebdev is providing best website development services from national to international clients. maniwebdev best website developer and designer. We work with passion and guarantee."
+          content="maniwebdev is providing best website development services from national to international clients. maniwebdev best website developer and designer. We work with passion and guarantee."
         />
         <meta
           name="keywords"
@@ -29,7 +28,6 @@ function index() {
           name="p:domain_verify"
           content="8cca891bf62b699c49c89a16eef4146a"
         />
-     
       </Head>
       <Navv />
       {/* section1 starts */}
@@ -61,7 +59,6 @@ function index() {
           <Image
             className="topimg"
             src="/images/homtop.png"
-            
             alt="maniwebdev Freelance website developer"
             width="420"
             height="420"
@@ -209,7 +206,71 @@ function index() {
           </div>
         </div>
       </div>
+      <div className="test">
+        <h3>Testimonials</h3>
+      </div>
+      <div className="cardmain">
+        <div className="card">
+          <Image
+            className="cardimg"
+            src="/images/girl.jpg"
+            alt="kay"
+            width="120"
+            height="120"
+          ></Image>
+          <span>
+            <h2 className="testih">Maria</h2>
+          </span>
+          <span>
+            <p className="testip">
+              I am very happy with the website that maniwebdev did for me. It
+              was completed before the deadline and my requested audits are
+              implemented very quickly, Thanks.
+            </p>
+          </span>
+        </div>
 
+        <div className="card2">
+          <Image
+            className="cardimg"
+            src="/images/man.jpg"
+            alt="kay"
+            width="120"
+            height="120"
+          ></Image>
+          <span>
+            <h2 className="testih">Mr. S</h2>
+          </span>
+          <span>
+            <p className="testip">
+              I have received my work on time. he has done this job quick and
+              i'm very satisified with his work so far. i will give him more
+              jobs for sure if he wants in the future. well done and thx a lot.
+            </p>
+          </span>
+        </div>
+
+        <div className="card3">
+          <Image
+            className="cardimg"
+            src="/images/jamatou.jpg"
+            alt="kay"
+            width="120"
+            height="120"
+          ></Image>
+          <span>
+            <h2 className="testih">Jamatou</h2>
+          </span>
+          <span>
+            <p className="testip">
+              Maniwebdev is absolutely the best. His responae time is very fast.
+              He is providing quality work. I am very happy with his service and
+              would recommend to everyone, Thanks.
+            </p>
+          </span>
+        </div>
+      </div>
+      <div className="revi"><p>NOTE! All These Reviews Are Available On My Profiles.</p></div>
       <div className="choose">
         <div className="cho">
           <h3>Why Choose Me?</h3>
