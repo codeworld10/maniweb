350232b13511cc8a6a55b8edc82fb504dd1a12eb
Muhammad Usman <manijnb0333@gmail.com>
2022-07-15 20:20:49 +0500

    design done

--- pages/global-worker-privacy-policy.js
+++ pages/global-worker-privacy-policy.js
@@ -3,8 +3,9 @@ import React from 'react'
 function global() {
   return (
     <div>
-    <p>
-    Privacy Policy
+    <p className='priv'>
+    <h1> Privacy Policy</h1>
+   
 Muhammad Usman built the Global Worker app as a Free app. This SERVICE is provided by Muhammad Usman at no cost and is intended for use as is.
 
 This page is used to inform visitors regarding my policies with the collection, use, and disclosure of Personal Information if anyone decided to use my Service.
@@ -26,13 +27,13 @@ Log Data
 
 I want to inform you that whenever you use my Service, in a case of an error in the app I collect data and information (through third-party products) on your phone called Log Data. This Log Data may include information such as your device Internet Protocol (“IP”) address, device name, operating system version, the configuration of the app when utilizing my Service, the time and date of your use of the Service, and other statistics.
 
-Cookies
+ <h2>Cookies </h2>
 
 Cookies are files with a small amount of data that are commonly used as anonymous unique identifiers. These are sent to your browser from the websites that you visit and are stored on your device's internal memory.
 
 This Service does not use these “cookies” explicitly. However, the app may use third-party code and libraries that use “cookies” to collect information and improve their services. You have the option to either accept or refuse these cookies and know when a cookie is being sent to your device. If you choose to refuse our cookies, you may not be able to use some portions of this Service.
 
-Service Providers
+ <h2>Service Providers</h2>
 
 I may employ third-party companies and individuals due to the following reasons:
 
@@ -42,7 +43,7 @@ To perform Service-related services; or
 To assist us in analyzing how our Service is used.
 I want to inform users of this Service that these third parties have access to their Personal Information. The reason is to perform the tasks assigned to them on our behalf. However, they are obligated not to disclose or use the information for any other purpose.
 
-Security
+<h2>Security</h2>
 
 I value your trust in providing us your Personal Information, thus we are striving to use commercially acceptable means of protecting it. But remember that no method of transmission over the internet, or method of electronic storage is 100% secure and reliable, and I cannot guarantee its absolute security.
 
@@ -50,7 +51,7 @@ Links to Other Sites
 
 This Service may contain links to other sites. If you click on a third-party link, you will be directed to that site. Note that these external sites are not operated by me. Therefore, I strongly advise you to review the Privacy Policy of these websites. I have no control over and assume no responsibility for the content, privacy policies, or practices of any third-party sites or services.
 
-Children’s Privacy
+<h2>Children’s Privacy</h2>
 
 These Services do not address anyone under the age of 13. I do not knowingly collect personally identifiable information from children under 13 years of age. In the case I discover that a child under 13 has provided me with personal information, I immediately delete this from our servers. If you are a parent or guardian and you are aware that your child has provided us with personal information, please contact me so that I will be able to do the necessary actions.
 
@@ -60,7 +61,7 @@ I may update our Privacy Policy from time to time. Thus, you are advised to revi
 
 This policy is effective as of 2022-07-15
 
-Contact Us
+<h2>Contact Us</h2>
 
 If you have any questions or suggestions about my Privacy Policy, do not hesitate to contact me at usman000033@gmail.com.
 
