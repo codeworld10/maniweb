84c65610d42c5bfe54c1064d814db1d9c221aef9
Muhammad Usman <manijnb0333@gmail.com>
2022-11-27 19:12:00 +0500

    terms

--- pages/global-worker-terms-conditions.js
+++ pages/global-worker-terms-conditions.js
@@ -15,7 +15,7 @@ const globalterms = () => {
         other intellectual property rights related to it, still belong to Usman
         mani.
         <p>
-          Usman mani is committed to ensuring that the app is as useful and
+          Muhammad Usman is committed to ensuring that the app is as useful and
           efficient as possible. For that reason, we reserve the right to make
           changes to the app or to charge for its services, at any time and for
           any reason. We will never charge you for the app or its services
@@ -32,10 +32,10 @@ const globalterms = () => {
           declare their Terms and Conditions. Link to Terms and Conditions of
           third-party service providers used by the app Google Play Services
           AdMob Expo You should be aware that there are certain things that
-          Usman mani will not take responsibility for. Certain functions of the
+          Muhammad Usman will not take responsibility for. Certain functions of the
           app will require the app to have an active internet connection. The
           connection can be Wi-Fi or provided by your mobile network provider,
-          but Usman mani cannot take responsibility for the app not working at
+          but Muhammad Usman cannot take responsibility for the app not working at
           full functionality if you don’t have access to Wi-Fi, and you don’t
           have any of your data allowance left. If you’re using the app outside
           of an area with Wi-Fi, you should remember that the terms of the
@@ -48,22 +48,22 @@ const globalterms = () => {
           country) without turning off data roaming. If you are not the bill
           payer for the device on which you’re using the app, please be aware
           that we assume that you have received permission from the bill payer
-          for using the app. Along the same lines, Usman mani cannot always take
+          for using the app. Along the same lines, Muhammad Usman cannot always take
           responsibility for the way you use the app i.e. You need to make sure
           that your device stays charged – if it runs out of battery and you
-          can’t turn it on to avail the Service, Usman mani cannot accept
-          responsibility. With respect to Usman mani’s responsibility for your
+          can’t turn it on to avail the Service, Muhammad Usman cannot accept
+          responsibility. With respect to Muhammad Usman’s responsibility for your
           use of the app, when you’re using the app, it’s important to bear in
           mind that although we endeavor to ensure that it is updated and
           correct at all times, we do rely on third parties to provide
-          information to us so that we can make it available to you. Usman mani
+          information to us so that we can make it available to you. Muhammad Usman
           accepts no liability for any loss, direct or indirect, you experience
           as a result of relying wholly on this functionality of the app. At
           some point, we may wish to update the app. The app is currently
           available on Android – the requirements for the system(and for any
           additional systems we decide to extend the availability of the app to)
           may change, and you’ll need to download the updates if you want to
-          keep using the app. Usman mani does not promise that it will always
+          keep using the app. Muhammad Usman does not promise that it will always
           update the app so that it is relevant to you and/or works with the
           Android version that you have installed on your device. However, you
           promise to always accept updates to the application when offered to
@@ -72,6 +72,10 @@ const globalterms = () => {
           we tell you otherwise, upon any termination, (a) the rights and
           licenses granted to you in these terms will end; (b) you must stop
           using the app, and (if needed) delete it from your device.
+        </p>
+        <p>
+        Before hiring a worker make sure the worker know about your task. This platform only make it easy for people to find work and worker in area. We are not compnay we are non-profit 
+        we do not give jobs. People in your area will give you the job. We are not responsible for any damage or loss. We are not responsible for any kind of fraud.
         </p>
         <h2>Changes to This Terms and Conditions</h2>
         <p>
