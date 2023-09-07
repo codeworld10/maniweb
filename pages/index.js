3ff6dfd055227ca37af935b75df0792619d6dc28
Muhammad Usman <manijnb0333@gmail.com>
2023-09-07 11:21:16 +0500

    more advance tracking

--- pages/index.js
+++ pages/index.js
@@ -120,14 +120,17 @@ const index = () => {
             <meta name="twitter:site" content="@maniwebdev" />
             <script async src="https://www.googletagmanager.com/gtag/js?id=G-TKRL2H1FM8"></script>
             <script
-              dangerouslySetInnerHTML={{
-                __html: `window.dataLayer = window.dataLayer || [];
-                         function gtag(){dataLayer.push(arguments);}
-                         gtag('js', new Date());
-            
-                         gtag('config', 'G-TKRL2H1FM8');
-                `,
-              }}
+                dangerouslySetInnerHTML={{
+                    __html: `window.dataLayer = window.dataLayer || [];
+                            function gtag(){dataLayer.push(arguments);}
+                            gtag('js', new Date());
+                            
+                            // Enable IP anonymization
+                            gtag('set', 'anonymizeIp', true);
+                            
+                            gtag('config', 'G-TKRL2H1FM8');
+                    `,
+                }}
             />
             {/* Canonical URL */}
             <link rel="canonical" href="https://maniwebdev.com" />
