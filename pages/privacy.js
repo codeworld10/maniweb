193cc83e9c40724e741697ae8c06ee9c5a5dfcaf
Muhammad Usman <manijnb0333@gmail.com>
2023-09-14 10:08:00 +0500

    privacy updated

--- pages/privacy.js
+++ pages/privacy.js
@@ -2,107 +2,93 @@ import Footer from "../Components/Footer";
 import Navv from "../Components/Navv";
 import Head from "next/head";
 
-
-const  privacy = () => {
+const PrivacyPolicy = () => {
   return (
     <>
-     <Head>
+      <Head>
         <title>Privacy Policy</title>
         <meta
           name="description"
-          content="Privacy policy of maniwebdev. Please read privacy policy of maniwebdev carefully before submitting the order. maniwebdev website developer."
+          content="Read the privacy policy of maniwebdev. Learn how we collect and use your information and your data protection rights. Maniwebdev website developer."
         />
         <meta
           name="keywords"
-          content="maniwebdev, maniwebdev privacy policy, maniwebdev website developer, website developer company, best freelance website developer, best website developer, best website designer, maniwebdev website developer,"
+          content="maniwebdev, privacy policy, website developer, freelance website developer, website designer"
         />
         <meta name="author" content="Usman" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        
       </Head>
-
-
 
       <Navv />
       <div className="pri_v">
         <h1>Privacy Policy</h1>
         <p>
-          At Mani Web dev, accessible from https://maniwebdev.com/, visitors
-          privacy is our main priority. This Privacy Policy document contains a
-          different type of information collected and recorded by Mani Web Dev
-          and how we use it? If you have additional questions or require more
-          information about our Privacy Policy, please contact us. General Data
-          Protection Regulation (GDPR) Mani Web Dev collects your information
-          for some particular purposes and ask for your consent to use your
-          provided information if it needs somewhere else. Mani Web Dev need to
-          perform a contract with you. You have permitted Mani Web Dev to do so
-          Processing your personal information is in Mani Web Dev, legitimate
-          interests. Mani Web dev need to comply with the law. Mani Web Dev will
-          retain your personal information only for as long as it is necessary
-          for the purposes set out in this Privacy Policy. We will retain and
-          use your information to the extent required to comply with our legal
-          obligations, resolve disputes, and enforce our policies. If you are a
+          At ManiWebDev, accessible from https://maniwebdev.com/, the privacy
+          and security of our visitors are our top priorities. This Privacy
+          Policy explains what information we collect and how we use it. If you
+          have any questions or need more information about our Privacy Policy,
+          please contact us.
+        </p>
+        <p>
+          <h2>Information Collection and Use</h2>
+          We collect your information for specific purposes and obtain your
+          consent when necessary. We may collect and use your information in
+          the following ways:
+          <ul>
+            <li>To perform a contract with you.</li>
+            <li>For our legitimate interests.</li>
+            <li>To comply with the law.</li>
+          </ul>
+          We will retain your personal information only as long as necessary to
+          fulfill these purposes or comply with legal obligations. If you are a
           European Economic Area resident (EEA), you have certain data
-          protection rights. If you wish to be informed of what Personal
-          Information we hold about you and if you want it to be removed from
-          our systems, please contact us. In certain circumstances, you have the
-          following data protection rights: The right to access, update or
-          delete the information we have on you. The right of rectification. The
-          right to object. The right of restriction. The right to data
-          portability The right to withdraw consent Log Files mani Web Dev
-          follow a standard procedure of using log files. These files log
-          visitors when they visit websites. All hosting companies do this and a
-          part of hosting services, analytics. The information collected by log
-          files includes internet protocol (IP) addresses, browser type,
-          Internet Service Provider (ISP), date and time stamp, referring/exit
-          pages, and possibly the number of clicks. These are not linked to any
-          personally identifiable information. The purpose of the information is
-          to analyze trends, administer the site, track users’ movement on the
-          website, and gather demographic information.
+          protection rights. You can contact us to access, update, or delete your
+          information or exercise other data protection rights.
+        </p>
+        <p>
+          <h2>Log Files</h2>
+          ManiWebDev follows standard log file procedures. Log files record
+          visitors' internet protocol (IP) addresses, browser types, Internet
+          Service Providers (ISPs), date and time stamps, and referring/exit
+          pages. This information is not linked to personally identifiable
+          information and is used for analysis and site administration.
         </p>
         <p>
           <h2>Our Advertising Partners</h2>
-          Some of the advertisers on our site may use cookies and web beacons.
-          Our advertising partners are listed below. Each of our advertising
-          partners has its own Privacy Policy for user data. For easier access,
-          we hyperlinked to their Privacy Policies below. Google
-          https://policies.google.com/technologies/ads. At Maniwebdev,
-          accessible from https://maniwebdev.com/, visitors privacy is our main
-          priority.You may consult this list to find the Privacy Policy for each
-          of Maniwebdev advertising partners. Third-party ad servers or ad
-          networks use technologies such as cookies, JavaScript, or Web Beacons
-          used in their respective advertisements. Also, links that appear on
-          Maniwebdev are sent directly to users browsers. They automatically
-          receive your IP address when this occurs. These technologies are used
-          to measure their advertising campaigns effectiveness and/or
-          personalize the advertising content you see on websites you visit.
-          <h3>Third-Party Privacy Policies</h3>
-          Maniwebdev Privacy Policy does not apply to other advertisers or
-          websites. Thus, we advise you to consult these third-party ad servers
-          respective Privacy Policies for more detailed information. It may
-          include their practices and instructions about how to opt-out of
-          specific options. You can choose to disable cookies through your
-          browser options. You can find out more about cookie management on the
-          browsers respective websites.
-          <h4> Consent</h4>
-          By using our website, you hereby consent to our Privacy Policy and
-          agree to its terms
+          Some advertisers on our site may use cookies and web beacons. Each
+          advertising partner has its Privacy Policy, and we provide links to
+          them for your reference. Third-party ad servers may use cookies and
+          web beacons to measure the effectiveness of their campaigns and
+          personalize advertising content. You can choose to disable cookies
+          through your browser settings.
         </p>
-        <div className="revsnnp"><h2>Revision Policy</h2> </div>
-      <div refu>
         <p>
-    
-          Customer satisfaction is our main goal and we will deliver website with 100% satisfaction and guarantee. But if there any mistake in
-          your website witnin 15 days you can contact with me and explain me the
-          problem what exactly happen. then I will try to fix the problem
-          immediately.
+          <h2>Third-Party Privacy Policies</h2>
+          ManiWebDev's Privacy Policy does not apply to other advertisers or
+          websites. We recommend reviewing the Privacy Policies of third-party
+          ad servers for more information on their practices and opt-out
+          options.
+        </p>
+        <p>
+          <h2>Consent</h2>
+          By using our website, you consent to our Privacy Policy and agree to
+          its terms.
         </p>
       </div>
+      <div className="revsnnp">
+        <h2>Revision Policy</h2>
       </div>
-     
+      <div className="refu">
+        <p>
+          Customer satisfaction is our primary goal. If you encounter any issues
+          with your website within 15 days, please contact us to explain the
+          problem. We will make every effort to promptly address and resolve any
+          issues to your satisfaction.
+        </p>
+      </div>
       <Footer />
     </>
   );
 }
 
-export default privacy;
+export default PrivacyPolicy;
